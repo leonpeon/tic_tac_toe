249cd34a85808ae3fe459a5b46786bf6cf579c14
leonpeon <59907683+leonpeon@users.noreply.github.com>
2026-07-24 16:28:05 +1000

    Update board based on a dictionary containing the player's turns

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 class Board:
     def __init__(self):
         self.space = "   "
-        self.column = "|"
-        self.row = "-------------"
+        self.wall = "|"
+        self.floor = "-----------"
+        self.row = (self.space + self.wall) * 2 + self.space
+        self.turn_dict = {}
+        for i in range(9):
+            self.turn_dict[i+1] = self.space
+        self.new_game = True
 
     def make_board(self):
-        print("\n")
-        for i in range(2):
-            row = (self.space + self.column) * 3
-            print(row, "\n", self.row)
-        print(row)
-        print("\n")
+        counter = 1
+        for i in range(17):
+            i += 1
+            if i % 2 == 1 or i == 1:
+                print(self.turn_dict[counter], end="")
+                counter += 1
+            else:
+                if (i + 1) % 6 == 0:
+                    print(self.wall)
+                elif i % 6 == 0:
+                    print(f"\n{self.floor}")
+                else:
+                    print(self.wall, end="")
+                    
 
 class Move:
     def __init__(self):
@@ -18,6 +31,7 @@ class Move:
             try:
                 self.guess = int(input("Which square? (1 - 9): "))
                 if 1 <= self.guess <= 9:
+                    Board.new_game = False
                     break
                 else:
                     print("Invalid input! Please input a number 1-9.")
