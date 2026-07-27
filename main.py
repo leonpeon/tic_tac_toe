5d2e1f93d4258e3d5a87eef758d1238aefb4d425
leonpeon <59907683+leonpeon@users.noreply.github.com>
2026-07-27 13:28:53 +1000

    Add comments

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ class Board:
         self.player_1_winner = False
         self.player_2_winner = False
 
+    # Creates the game board. It keeps track of which spaces have already been played by using the turn_dict.
     def make_board(self):
         counter = 1
         print("\n")
@@ -32,20 +33,24 @@ class Board:
 
     def check_combo(self):
         add_num = 1
+        # All the winning combinations start with these numbers.
         winning_numbers = [1, 2, 3, 4, 7]
-        start_num_dict = {
+        # The keys are the numbers to add the numbers in the list by. E.g. If the key is 2, then 3, 5, 7 is a winning combo.
+        add_num_dict = {
             1: [1, 4, 7],
             2: [3],
             3: [1, 2, 3],
             4: [1]
         }
+        # Checks if any of the winning combos have been fulfilled.
         while add_num <= 4:
             for i in winning_numbers:
-                if i in start_num_dict[add_num]:
-                    for item in start_num_dict[add_num]:
+                if i in add_num_dict[add_num]:
+                    for item in add_num_dict[add_num]:
                         current_combo = [self.turn_dict[item], 
                                         self.turn_dict[item+add_num], 
                                         self.turn_dict[item+(add_num*2)]]
+                        # Checks if a winning combo has three X's or O's
                         if not self.check_win(current_combo):
                             pass
                         else:
@@ -53,6 +58,7 @@ class Board:
                             break
             add_num += 1
 
+    # Checks if a winning combo has three X's or O's
     def check_win(self, combo):
         if all(move == " O " for move in combo):
             self.player_1_winner = True
@@ -63,6 +69,7 @@ class Board:
         else:
             return False
 
+    # Resets the board if the player wants to play again.
     def reset_board(self):
         self.game_over = False
         self.player_1_winner = False
@@ -72,16 +79,19 @@ class Board:
         
 board = Board()
 
+# Controls the player's/computer's moves.
 class Move:
     def __init__(self):
         self.turn_count = 1
 
     def player_turn(self, cpu_human, cpu_mode):
+        # Checks if the player wants to play against a CPU or Player 2.
         if cpu_human == 1:
             cpu = True
         else:
             cpu = False
-        
+
+        # Asks player to make a move, and automatically makes the CPU move once the player has chosen a move.
         if self.turn_count <= 9:
             try:
                 if cpu:
@@ -110,6 +120,7 @@ class Move:
             except ValueError:
                 print("Invalid input! Please input a number 1-9.")
 
+            # Checks if a player has won, or if there is draw.
             if board.game_over:
                 if board.player_1_winner:
                     print("PLAYER 1 WINS!\n")
@@ -120,6 +131,7 @@ class Move:
                 print("DRAW!")
                 board.game_over = True
 
+# Easy mode: the CPU choses a random space in out of all available spaces.
     def cpu_move_easy(self):
         turn_dict = board.turn_dict
         valid_moves = list(turn_dict.keys())
@@ -131,13 +143,15 @@ class Move:
         print(valid_moves)
         return(cpu_move)
 
+# Hard mode: the CPU will block any spaces that allow the player to win the next turn.
+#            Otherwise, the CPU will choose a move which sets itself up for a win.
+# NEED TO IMPLEMENT
     def cpu_move_hard(self):
-        # Needs to check the moves which would allow for the player to win,
-        # and then moves which allows for the CPU to win.
         pass
 
 move = Move()
 
+# Asks the user how they would like to play the game.
 while True:
     cpu_or_human = int(input("Do you want to play against the CPU or Player 2? (1 or 2): "))
     cpu_mode = None
@@ -153,6 +167,7 @@ while True:
                     break
         break
 
+# Keeps looping the game as long as the user wishes to play.
 while board.game_over is False:
         if cpu_or_human == 1 or cpu_or_human == 2:
             board.make_board()
